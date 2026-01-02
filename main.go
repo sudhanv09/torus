e5de200e4f245695308754427d0472459e24f306
sudhanv09 <mystogun125@gmail.com>
2026-01-02 16:56:21 +0800

    vibe coded FE

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"log"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
+	"github.com/a-h/templ"
+	"net/http"
+	"sudhanv09/torus/view"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %v", err)
+	// }
+
+	dashboard := view.Dashboard()
+	http.Handle("/", templ.Handler(dashboard))
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
